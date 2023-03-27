612c16f5871dd4ac388d34fe004333707c67b369
victorli112 <victor.li.7802@gmail.com>
2023-03-26 20:44:39 -0400

    boss health bar

---
+++ Assets/Scripts/BossHealth.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossHealth : MonoBehaviour
+{
+
+	public int maxHealth = 500;
+	public int currentHealth;
+	public HealthBar healthBar;
+
+	void Start()
+	{
+		healthBar.SetMaxHealth(maxHealth);
+		currentHealth = maxHealth;
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			TakeDamage(20);
+		}
+	}
+
+	public void TakeDamage(int damage)
+	{
+		currentHealth -= damage;
+		healthBar.SetHealth(currentHealth);
+
+		// if (health <= 0)
+		// {
+		// 	Die();
+		// }
+	}
+
+	// void Die()
+	// {
+	// 	Instantiate(deathEffect, transform.position, Quaternion.identity);
+	// 	Destroy(gameObject);
+	// }
+
+}
